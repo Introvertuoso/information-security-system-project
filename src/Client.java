27f1c12c97fee3937614da75d54b33656fb56939
Jawad <48954771+introvertuoso@users.noreply.github.com>
2020-11-27 22:28:59 +0200

    * Added Multithreading to the Server

--- src/Client.java
+++ src/Client.java
@@ -5,6 +5,7 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
@@ -13,15 +14,26 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Scanner;
 
-
 public class Client {
     private static final String key = "aesEncryptionKey";
     private static final String initVector = "encryptionIntVec";
 
-    public static void main(String[] args) throws IOException, NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
-        Socket socket = new Socket("127.0.0.1", 11111);
-        Scanner in = new Scanner(socket.getInputStream());
-        System.out.println("Server response: " + decrypt(in.nextLine()));
+    public static void main(String[] args) {
+//        TODO: obtain server IP and port as inputs
+//        TODO: obtain intended file name as input
+//        TODO: support read & write on the provided file
+        try (Socket socket = new Socket("127.0.0.1", 11111)) {
+            System.out.println("Enter lines of text then Ctrl+D or Ctrl+C to quit");
+            Scanner scanner = new Scanner(System.in);
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            while (scanner.hasNextLine()) {
+                out.println(scanner.nextLine());    // Sends user input on the socket output stream
+                System.out.println(decrypt(in.nextLine()));  // Displays server response from socket input stream
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     private static String decrypt(String data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
---
+++ src/ConnectionHandler.java
@@ -0,0 +1,64 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// nc localhost 11111
+public class ConnectionHandler implements Runnable{
+    private final Socket socket;
+    private String data;
+    private static final String initVector = "encryptionIntVec";
+    private static final String key = "aesEncryptionKey";
+
+    ConnectionHandler(Socket socket) {
+        this.data = "";
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Connected: " + socket);
+        try {
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            while (in.hasNextLine()) {
+                data = in.nextLine();
+                out.println(encrypt(data));
+            }
+        } catch (Exception e) {
+            System.out.println("Error:" + socket);
+        } finally {
+            try { socket.close(); } catch (IOException e) {}
+            System.out.println("Closed: " + socket);
+        }
+    }
+
+    private static String encrypt(String data) throws InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException {
+        IvParameterSpec iv = new IvParameterSpec(initVector.getBytes());
+        SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), "AES");
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv); // or Cipher.DECRYPT_MODE
+
+        byte[] encrypted = cipher.doFinal(data.getBytes());
+
+        String s = Base64.getEncoder().encodeToString(encrypted);
+        System.out.println(s);
+        return s;
+    }
+
+}
